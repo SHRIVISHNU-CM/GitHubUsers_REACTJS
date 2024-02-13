71164eac7c693e88202097b6918b212b27c0f7c7
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-13 20:44:37 +0530

    correcting error and adding few features

--- gituser/src/App.jsx
+++ gituser/src/App.jsx
@@ -1,41 +1,72 @@
-
-
+import { useState, useEffect } from "react"
+import axios from "axios"
 function GitUser() {
-  const USER_API = "https://api.github.com/users/"
+
+  const [username, setUsername] = useState("")
+  const [userData, setUserData] = useState({});
+
+  const Submit =()=>{
+    axios.get(  `https://api.github.com/users/${username}`)
+      .then((res) => {
+        console.log(res.data)
+        setUserData(res.data);
+      })
+  }
 
   return (
     <>
 
-      <div className="flex justify-center border items-center">
+      <div className="flex justify-center border items-center gap-2">
         <input
-
+          value={username}
           type="text"
+          placeholder="Enter Username"
           className="border p-2 rounded-lg shadow-2xl border-blue-600"
+          onChange={(e) => {
+
+            setUsername(e.target.value)}}
         />
         <button
+        onClick={Submit}
           className="p-2 bg-green-600 text-white font-semibold rounded-md"
         >Search</button>
       </div>
-      <div className="flex mt-2 justify-center">
-        <div className="border w-[400px] h-[500px] bg-stone-700 p-1">
+      <div className="flex mt-2 justify-center ">
+        <div className="border w-[450px]  bg-stone-700 p-3 h-min">
           <div className="flex justify-around items-center mb-2">
-            <img className="bg-blue-400 rounded-[100%] w-[200px] h-[200px]" />
+            <img className="bg-blue-400 rounded-[100%] w-[200px] h-[200px] overflow-hidden" src={userData.avatar_url} />
             <div className="flex flex-col items-center">
               <h1 className="text-white">Name</h1>
-              <h1 className="text-white">Vishnu</h1>
+              <h1 className="text-white">{userData.name==null? userData.login:userData.name}</h1>
             </div>
 
           </div>
           <div className="flex justify-around">
-            <div className="border w-24 text-center">
+            <div className="border w-24 text-center p-1">
               <h2>Followers</h2>
-              <h3 className="border-t">3</h3>
+              <h3 className="border-t text-white">{userData.followers}</h3>
             </div>
-            <div className="border w-24 text-center">
+            <div className="border w-34 text-center p-1">
               <h2>Follwing</h2>
-              <h3 className="border-t">12</h3>
+              <h3 className="border-t text-white">{userData.following}</h3>
             </div>
           </div>
+          <div className="flex justify-around">
+            <div className="border w-34 text-center p-1">
+              <h2>Public Repositories</h2>
+              <h3 className="border-t text-white">{userData.public_repos}</h3>
+            </div>
+            <div className="border w-34 text-center p-1">
+              <h2>Location</h2>
+              <h3 className="border-t text-white">{userData.location==null? "N/A":userData.location}</h3>
+            </div>
+          </div>
+          {userData.bio==null?<></>:
+            <div className="text-center border h-min">
+              <p>{userData.bio}</p>
+            </div>
+          }
+          
 
         </div>
       </div>
@@ -44,4 +75,4 @@ function GitUser() {
   )
 }
 
-export default GitUser;
+export default GitUser
